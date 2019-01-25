b8c4aebe322984a30b64fe335ce4bd367a06b9dd
Filipe Pereira <36936726+FilipePereiraTSIW@users.noreply.github.com>
2019-01-25 10:27:17 +0000

    first commit

---
+++ Projeto/index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows : true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+
+}
